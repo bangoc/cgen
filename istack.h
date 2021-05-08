3355601c3e4fedd09e43dd4f562ae29311bb9178
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-05-08 23:34:19 +0700

    Implement integer stack

--- istack.h
+++ istack.h
@@ -1,24 +1,24 @@
-#ifndef IQUEUE_H_
-#define IQUEUE_H_
+#ifndef ISTACK_H_
+#define ISTACK_H_
 
 #include "gtl1.h"
 
 // ========== Khai báo hàm ===============
 
-static list1_t iqu_enqueue(list1_t list, long value);
-static long iqu_dequeue(list1_t list);
-static long iqu_peek(list1_t list);
+static list1_t itk_push(list1_t list, long value);
+static long itk_pop(list1_t list);
+static long itk_top(list1_t list);
 
 // ========== Macro viết nhanh ===========
 
 
 // ========== Định nghĩa hàm =============
 
-static list1_t iqu_enqueue(list1_t list, long value) {
-  return gtl_append(list, gtl_make_node((gtype){.i = value}));
+static list1_t itk_push(list1_t list, long value) {
+  return gtl_prepend(list, gtl_make_node((gtype){.i = value}));
 }
 
-static long iqu_dequeue(list1_t list) {
+static long itk_pop(list1_t list) {
   gtl_node_t node = gtl_remove_first(list);
   if (node) {
     long value = node->val.i;
@@ -28,11 +28,11 @@ static long iqu_dequeue(list1_t list) {
   return INVALID_VALUE.i;
 }
 
-static long iqu_peek(list1_t list) {
+static long itk_top(list1_t list) {
   if (list && list->first) {
     gtl_node_value(list).i;
   }
   return INVALID_VALUE.i;
 }
 
-#endif  // IQUEUE_H_
+#endif  // ISTACK_H_
