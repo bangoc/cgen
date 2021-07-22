b19244b8af6aef6b4726587890dc4b962b49b662
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-07-22 23:05:41 +0700

    Kiểm thử thêm & xóa 1000 giá trị vào cây đỏ-đen

---
+++ tests/rb/rb_insert_delete_1k_ut.c
@@ -0,0 +1,30 @@
+#include "rbi.h"
+#include "rbi_helper.h"
+#include "tests/base/bn_utils.h"
+#include "tests/base/utils.h"
+
+#define N 1000
+
+int main() {
+  int a[N];
+  for (int i = 0; i < N; ++i) {
+    a[i] = i;
+  }
+  bn_tree_t t = bn_create_tree(NULL_PTR);
+
+  // insert
+  for (int i = 0; i < N; ++i) {
+    rbi_insert(t, a[i]);
+    CHECK_MSG(lnr_match_value(t, a, i + 1), "Lỗi giá trị");
+    CHECK_MSG(rb_is_valid(t), "Lỗi cây đỏ-đen");
+  }
+
+  // delete
+  for (int i = N - 1; i >= 0; --i) {
+    rbi_delete(t, a[i]);
+    CHECK_MSG(lnr_match_value(t, a, i), "Lỗi xóa giá trị");
+    CHECK_MSG(rb_is_valid(t), "Lỗi cây đỏ-đen");
+  }
+  printf("rb_insert_delete_1k Ok\n");
+  return 0;
+}
