9b30a7bf90e4b97acb49cdb124aa53eaf0332b76
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-07-22 14:05:40 +0700

    Kiểm thử: Xóa nút đen có nút đối xứng đen, đỉnh đen, không có nút con

--- tests/rb/rb_delete_similar_ut.c
+++ tests/rb/rb_delete_similar_ut.c
@@ -221,6 +221,84 @@ int delete_black_node_no_child_red_sibling_black_top() {
   return 0;
 }
 
+int delete_black_node_no_child_black_sibling_black_top1() {
+  bn_tree_t t = make_tree2();
+  rbi_delete(t, 1);
+  /*
+               5B
+         2B           7B
+             3B   6B      9R
+                        8B   10B
+             >>>>>
+           p->  5B
+     n-> 2B           7B    <- s
+             3R   6B      9R
+                        8B   10B
+              >>>>>
+                 7B
+            5B       9B
+        2B     6B  8B  10B
+          3R
+  */
+  rbi_node_t n2  = rbi_create_color_node(2,  RB_BLACK);
+  rbi_node_t n3  = rbi_create_color_node(3,  RB_RED);
+  rbi_node_t n5  = rbi_create_color_node(5,  RB_BLACK);
+  rbi_node_t n6  = rbi_create_color_node(6,  RB_BLACK);
+  rbi_node_t n7  = rbi_create_color_node(7,  RB_BLACK);
+  rbi_node_t n8  = rbi_create_color_node(8,  RB_BLACK);
+  rbi_node_t n9  = rbi_create_color_node(9,  RB_BLACK);
+  rbi_node_t n10 = rbi_create_color_node(10, RB_BLACK);
+  bn_connect2(n2, right, n3, top);
+  bn_connect2(n5, left, n2, top);
+  bn_connect2(n5, right, n6, top);
+  bn_connect2(n7, left, n5, top);
+  bn_connect2(n7, right, n9, top);
+  bn_connect2(n9, left, n8, top);
+  bn_connect2(n9, right, n10, top);
+  bn_tree_t s = bn_create_tree(to_bn(n7));
+  CHECK_MSG(bn_similar_tree(t, s, rbi_similar_node), "Cây sau khi xóa 1");
+  return 0;
+}
+
+int delete_black_node_no_child_black_sibling_black_top2() {
+  bn_tree_t t = make_tree2();
+  rbi_delete(t, 3);
+  /*
+               5B
+         2B           7B
+      1B          6B      9R
+                        8B   10B
+             >>>>>
+        p->    5B
+     n->  2B      s -> 7B
+      1R          6B       9R
+                         8B   10B
+              >>>>>
+                 7B
+            5B       9B
+        2B     6B  8B  10B
+      1R
+  */
+  rbi_node_t n1  = rbi_create_color_node(1,  RB_RED);
+  rbi_node_t n2  = rbi_create_color_node(2,  RB_BLACK);
+  rbi_node_t n5  = rbi_create_color_node(5,  RB_BLACK);
+  rbi_node_t n6  = rbi_create_color_node(6,  RB_BLACK);
+  rbi_node_t n7  = rbi_create_color_node(7,  RB_BLACK);
+  rbi_node_t n8  = rbi_create_color_node(8,  RB_BLACK);
+  rbi_node_t n9  = rbi_create_color_node(9,  RB_BLACK);
+  rbi_node_t n10 = rbi_create_color_node(10, RB_BLACK);
+  bn_connect2(n2, left, n1, top);
+  bn_connect2(n5, left, n2, top);
+  bn_connect2(n5, right, n6, top);
+  bn_connect2(n7, left, n5, top);
+  bn_connect2(n7, right, n9, top);
+  bn_connect2(n9, left, n8, top);
+  bn_connect2(n9, right, n10, top);
+  bn_tree_t s = bn_create_tree(to_bn(n7));
+  CHECK_MSG(bn_similar_tree(t, s, rbi_similar_node), "Cây sau khi xóa 3");
+  return 0;
+}
+
 int main() {
   CHECK_MSG(delete_node_one_left_child() == 0, "Sai ở delete_node_one_left_child()");
   CHECK_MSG(delete_node_one_right_child() == 0, "Sai ở delete_node_one_right_child()");
@@ -231,5 +309,9 @@ int main() {
         "Sai ở delete_black_node_no_child_red_top_black_sibling2()");
   CHECK_MSG(delete_black_node_no_child_red_sibling_black_top() == 0,
         "Sai ở delete_black_node_no_child_red_sibling_black_top()");
+  CHECK_MSG(delete_black_node_no_child_black_sibling_black_top1() == 0,
+        "Sai ở delete_black_node_no_child_black_sibling_black_top1()");
+  CHECK_MSG(delete_black_node_no_child_black_sibling_black_top2() == 0,
+        "Sai ở delete_black_node_no_child_black_sibling_black_top2()");
   return 0;
 }
