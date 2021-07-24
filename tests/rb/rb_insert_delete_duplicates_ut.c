77699da8aedadb428f3441e4966f41609a3d03c8
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-07-24 13:16:45 +0700

    Kiểm thử thêm & xóa giá trị trùng lặp trong cây đỏ-đen

---
+++ tests/rb/rb_insert_delete_duplicates_ut.c
@@ -0,0 +1,24 @@
+#include "tests/base/helpers.h"
+#include "tests/base/utils.h"
+
+#include "rbi.h"
+
+int main() {
+  int a[] = {1, 2, 2, 3, 3, 3, 5, 5, 5, 5, 5, 6, 6, 6, 6, 8, 9, 10};
+  int n = sizeof(a)/sizeof(a[0]);
+  bn_tree_t t = bn_create_tree(NULL_PTR);
+  for (int i = 0; i < n; ++i) {
+    rbi_insert(t, a[i]);
+    CHECK_MSG(lnr_match_value(t, a, i + 1), "Giá trị sau khi thêm vào.");
+    CHECK_MSG(rb_is_valid(t), "Tính chất đỏ-đen sau khi thêm giá trị.");
+  }
+  bn_pprint(t, rbi_print_node);
+  for (int i = n - 1; i >= 0; --i) {
+    rbi_node_t node = rbi_delete(t, a[i]);
+    CHECK_MSG(node->value == a[i], "Giá trị hàm xóa trả về.");
+    CHECK_MSG(lnr_match_value(t, a, i), "Các giá trị còn lại sau khi xóa.");
+    CHECK_MSG(rb_is_valid(t), "Tính chất đỏ-đen sau khi xóa.");
+  }
+  printf("insert_delete_duplicates ok\n");
+  return 0;
+}
