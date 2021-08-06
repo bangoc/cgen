2e25600773e9c98b0f129af459b116617d842b0f
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-08-06 14:20:54 +0700

    Kiểm thử hiệu năng cây đỏ đen

---
+++ examples/benchmarks/rb/bench_123.c
@@ -0,0 +1,53 @@
+#include "examples/benchmarks/bench.h"
+#include "tests/rb/rbi.h"
+
+#include <stdio.h>
+
+#define MIN 100000l
+#define POINTS 10l
+
+int usage() {
+  printf("./Usage: ./p 10000000\nTham số phải > %ld và chia hết cho %ld.\n", MIN, POINTS);
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    return usage();
+  }
+  long n;
+  sscanf(argv[1], "%ld", &n);
+  if (n <= MIN || n % 10 != 0) {
+    return usage();
+  }
+  const long len = n / POINTS;
+  char desc[1024];
+  long value = 0;
+  bn_tree_t t = bn_create_tree(NULL_PTR);
+  for (long point = 0; point < POINTS; ++point) {
+    sprintf(desc, "%ld x insert from %ld (s): ", len, len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              rbi_insert(t, value++);
+            }
+          );
+    sprintf(desc, "%ld x search from %ld (s): ", len, len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              rbi_search(t, len * point + i);
+            }
+          );
+  }
+
+  // xóa
+  value = 0;
+  for  (long point = 0; point < POINTS; ++point) {
+    sprintf(desc, "%ld x delete from %ld (s): ", len, n - len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              rbi_delete(t, value++);
+            }
+          );
+  }
+  return 0;
+}
---
+++ examples/benchmarks/rb/bench_123.cc
@@ -0,0 +1,53 @@
+#include "examples/benchmarks/bench.h"
+
+#include <stdio.h>
+#include <set>
+
+#define MIN 100000l
+#define POINTS 10l
+
+int usage() {
+  printf("./Usage: ./p 10000000\nTham số phải > %ld và chia hết cho %ld.\n", MIN, POINTS);
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    return usage();
+  }
+  long n;
+  sscanf(argv[1], "%ld", &n);
+  if (n <= MIN || n % 10 != 0) {
+    return usage();
+  }
+  const long len = n / POINTS;
+  char desc[1024];
+  long value = 0;
+  std::set<long> t;
+  for (long point = 0; point < POINTS; ++point) {
+    sprintf(desc, "%ld x insert from %ld (s): ", len, len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              t.insert(value++);
+            }
+          );
+    sprintf(desc, "%ld x search from %ld (s): ", len, len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              t.find(len * point + i);
+            }
+          );
+  }
+
+  // xóa
+  value = 0;
+  for  (long point = 0; point < POINTS; ++point) {
+    sprintf(desc, "%ld x delete from %ld (s): ", len, n - len * point);
+    BENCH(desc,
+            for (long i = 0; i < len; ++i) {
+              t.erase(value++);
+            }
+          );
+  }
+  return 0;
+}
