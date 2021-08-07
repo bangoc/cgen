7c31e63a08942808dba4d77a88dcf86aa5d15a89
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-08-07 23:37:57 +0700

    Thêm kiểm thử search_gte và search_lte

---
+++ tests/bns/bns_search_lte_gte_1k_ut.c
@@ -0,0 +1,23 @@
+#include "bns.h"
+
+#include "tests/base/utils.h"
+#include "tests/rb/rbi.h"
+
+#define N 1000
+
+int main() {
+  bn_tree_t t = bn_create_tree(NULL_PTR);
+  // 0, 2, ..., 2 * N - 2
+  for (int i = 0; i < N; ++i) {
+    rbi_insert(t, 2  * i);
+  }
+  for (int i = 0; i < N - 1; ++i) {
+    int value = 2 * i + 1;
+    bn_node_t out;
+    bns_search_gte_inline(out, t->root, value, rbi_compare_data);
+    CHECK_MSG(out && rbi_value(out) == value + 1, "search gte with odd number");
+    bns_search_lte_inline(out, t->root, value, rbi_compare_data);
+    CHECK_MSG(out && rbi_value(out) == value - 1, "search lte with odd number");
+  }
+  return 0;
+}
