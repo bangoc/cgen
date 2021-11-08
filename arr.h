eecce5daed176b9465b2cc23add3df56e470aa46
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-11-08 19:05:48 +0700

    Mảng cấp phát động khái quát

---
+++ arr.h
@@ -0,0 +1,93 @@
+#ifndef ARR_H_
+#define ARR_H_
+
+/*
+  (C) Nguyen Ba Ngoc 2021
+*/
+
+#include <stdlib.h>
+
+#define ARR(pa) (*pa)
+
+enum arr_attrib {
+  ARR_SZ = 0,
+  ARR_CAP,
+  ARR_ELEM_SZ,
+  ARR_INC,
+  ARR_ATT_MAX
+};
+
+static inline void **arr_create_internal(size_t cap, size_t elem_sz) {
+  size_t* a = malloc(ARR_ATT_MAX * sizeof(size_t) + cap * elem_sz);
+  if (a) {
+    a[ARR_SZ] = 0;
+    a[ARR_CAP] = cap;
+    a[ARR_ELEM_SZ] = elem_sz;
+    a[ARR_INC] = 30;  // %
+    void **tmp = malloc(sizeof(void*));
+    *tmp = a + ARR_ATT_MAX;
+    return tmp;
+  }
+  return NULL;
+}
+
+#define ARR_BEG(a) (((size_t*)ARR(a)) - ARR_ATT_MAX)
+#define arr_size(a) (ARR_BEG(a)[ARR_SZ])
+#define arr_cap(a) (ARR_BEG(a)[ARR_CAP])
+#define arr_elem_sz(a) (ARR_BEG(a)[ARR_ELEM_SZ])
+#define arr_inc(a) (ARR_BEG(a)[ARR_INC])
+
+#define arr_free(a) \
+  do { \
+    free(ARR_BEG(a)); \
+    *a = NULL; \
+    free(a); \
+    a = NULL; \
+  } while (0)
+
+#define arr_set_size(a, newsize) \
+  do { \
+    if (newsize < arr_cap(a)) { \
+      arr_size(a) = newsize; \
+    } \
+  } while (0)
+
+#define arr_set_capacity(a, newcap) \
+  do { \
+    size_t *tmp = ARR_BEG(a); \
+    size_t elem_sz = tmp[ARR_ELEM_SZ]; \
+    tmp = realloc(tmp, ARR_ATT_MAX * sizeof(size_t) + newcap * elem_sz); \
+    if (tmp) { \
+      if (tmp[ARR_SZ] > newcap) { \
+        tmp[ARR_SZ] = newcap; \
+      } \
+      tmp[ARR_CAP] = newcap; \
+      (*((void**)a)) = (void*)(tmp + ARR_ATT_MAX); \
+    } \
+  } while (0)
+
+#define arr_create(cap, etyp) \
+   arr_create_internal(cap, sizeof(etyp))
+
+#define arr_t(etyp) etyp **
+
+#define arr_decl(name, cap, etyp) \
+    etyp **name = arr_create(cap, etyp);
+
+#define arr_append(a, elem) \
+   do { \
+     size_t *tmp = ARR_BEG(a); \
+     if (tmp[ARR_SZ] >= tmp[ARR_CAP]) { \
+       size_t c = tmp[ARR_CAP]; \
+       size_t inc = tmp[ARR_INC]; \
+       size_t newcap = c > 100? c + c * inc / 100: c + inc; \
+       arr_set_capacity(a, newcap); \
+       tmp = ARR_BEG(a); \
+     } \
+     if (tmp[ARR_SZ] < tmp[ARR_CAP]) { \
+       (*a)[tmp[ARR_SZ]] = elem; \
+       ++(tmp[ARR_SZ]); \
+     } \
+   } while (0)
+
+#endif  // ARR_H_
---
+++ tests/arr/arr_i_ut.c
@@ -0,0 +1,34 @@
+/*
+  (C) Nguyen Ba Ngoc 2021
+  Minh họa mảng giá trị int cấp phát động.
+*/
+
+#include "arr.h"
+
+#include "tests/base/utils.h"
+
+#include <stdio.h>
+
+int t1() {
+  int **a = (int**)arr_create(10, int);
+  ASSERT(arr_size(a) == 0, "init size = 0");
+  ASSERT(arr_cap(a) == 10, "init cap = 0");
+  ASSERT(arr_elem_sz(a) == sizeof(int), "elem sz");
+  const int n = 100;
+  for (int i = 0; i < n; ++i) {
+    arr_append(a, i);
+  }
+  ASSERT(arr_size(a) == n, "append n elements");
+  ASSERT(arr_cap(a) >= n, "capacity changed");
+  for (size_t i = 0; i < arr_size(a); ++i) {
+    ASSERT((*a)[i] == i, "Value");
+  }
+  arr_free(a);
+  return 0;
+}
+
+int main() {
+  ASSERT(t1() == 0, "t1()");
+  printf("Test Ok\n");
+  return 0;
+}
