a064c3d073fbaeb00625e91c157491080a6e32bf
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-12-20 18:03:08 +0700

    Kiểm thử xóa phần tử khỏi gvec

--- tests/gvec/gvec_ut.c
+++ tests/gvec/gvec_ut.c
@@ -34,6 +34,30 @@ int t2() {
   gvec_append(v, gtype_s(strdup("BBB")));
   gvec_qsort(v, gtype_inc_cmp_s);
   CHECK_MSG(gtype_seqs(gvec_arr(v), (char*[]){"AAA", "BBB", "CCC"}, 3), "sorted strings");
+  gvec_remove(v, 2);
+  CHECK_MSG(gtype_seqs(gvec_arr(v), (char*[]){"AAA", "BBB"}, 2), "2 sorted strings");
+  CHECK_MSG(gvec_size(v) == 2, "size 2");
+  gvec_free(v);
+  return 0;
+}
+
+int t3() {
+  gvec_t v = gvec_create(3, NULL);
+  for (int i = 0; i < 10; ++i) {
+    gvec_append(v, gtype_i(i));
+  }
+  CHECK_MSG(gvec_size(v) == 10, "gvec size 10");
+  CHECK_MSG(gtype_seqi(gvec_arr(v), (int[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 10), "10 seq");
+  gvec_remove(v, 11);
+  gvec_remove(v, 8);
+  gvec_remove(v, 4);
+  gvec_remove(v, 1);
+  CHECK_MSG(gtype_seqi(gvec_arr(v), (int[]){0, 2, 3, 5, 6, 7, 9}, 7), "7 seq");
+  CHECK_MSG(gvec_size(v) == 7, "size 7");
+  gvec_remove(v, 0);
+  gvec_remove(v, 0);
+  CHECK_MSG(gvec_size(v) == 5, "size 5");
+  CHECK_MSG(gtype_seqi(gvec_arr(v), (int[]){3, 5, 6, 7, 9}, 5), "5 seq");
   gvec_free(v);
   return 0;
 }
@@ -41,5 +65,6 @@ int t2() {
 int main() {
   CHECK_MSG(t1() == 0, "t1()");
   CHECK_MSG(t2() == 0, "t2()");
+  CHECK_MSG(t3() == 0, "t3()");
   TEST_OK();
 }
