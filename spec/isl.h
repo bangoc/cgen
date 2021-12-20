989f208cb47d706cb8c6b5109fa991a8ec56dff7
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-12-20 15:20:15 +0700

    Tham số isl

--- spec/isl.h
+++ spec/isl.h
@@ -14,7 +14,7 @@ typedef struct isn_s {
 void isn_pprint(sln_t node);
 void isl_pprint(sll_t list);
 
-#define to_isn(n) ((isn_t)n)
+#define to_isn(n) ((isn_t)(n))
 #define isn_value(n) (to_isn(n)->value)
 
 sln_t isn_create(long value);
