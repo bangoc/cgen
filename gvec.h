e503dd64447de91e7655c34230e46ad00fa4a827
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-01-12 00:19:40 +0700

    doxygen cho gvec_t

--- gvec.h
+++ gvec.h
@@ -1,22 +1,72 @@
 #ifndef GVEC_H_
 #define GVEC_H_
 
-/*
-  (C) Nguyen Ba Ngoc 2021
-  gtype vector
-*/
+/* (C) Nguyen Ba Ngoc 2021 */
+
+/** @file
+ * @brief Vec-tơ (mảng cấp phát động) của các giá trị gtype.
+ *
+ * Có thể truy cập ngẫu nhiên đến 1 phần tử bất kỳ trong vec-tơ
+ * theo chỉ số và sắp xếp nhanh vec-tơ tương tự như mảng.
+ * Tuy nhiên kích thước của vec-tơ có thể thay đổi sau khi được tạo
+ * (dựa trên realloc) vì vậy linh động hơn mảng thông thường và
+ * cũng có phạm vi ứng dụng rộng hơn.
+ */
 
 #include "base/arr.h"
 #include "base/gtype.h"
 
+/**
+ * Cấu trúc lưu các thông tin điều khiển vec-tơ.
+ *
+ * Các macros điều khiển:
+ *
+ *   #gvec_size(v) - Trả về size của vec-tơ.
+ *
+ *   #gvec_capacity(v) - Trả về capacity của vec-tơ.
+ */
 typedef struct gvec_s {
+  /**
+   * Mảng cơ sở lưu các đối tượng gtype
+   */
   arr_t(gtype) arr;
+
+  /**
+   * Con trỏ tới hàm giải phóng dữ liệu được quản lý qua đối tượng
+   * gtype trong mảng. Nếu != NULL thì hàm được gọi khi giải phóng
+   * bộ nhớ của vec-tơ, nếu ngược lại (== NULL) thì các gọi hàm được
+   * bỏ qua.
+   */
   gtype_free_t free_value;
 } *gvec_t;
 
+/**
+ * Hàm tạo đối tượng vec-tơ.
+ *
+ * @param cap (capacity) Dung lượng ban đầu của vec-tơ.
+ * @param free_value con trỏ hàm giải phóng bộ nhớ bên ngoài đối tượng gtype.
+ * Sử dụng NULL nếu không có bộ nhớ bên ngoài.
+ *
+ * \memberof gvec_s
+ */
 gvec_t gvec_create(int cap, gtype_free_t free_value);
 
+/**
+ * Trả về kích thước (size) của vec-tơ. Trong vec-tơ size là
+ * số lượng phần tử mảng đã sử dụng, còn capacity là số lượng phần tử
+ * đã cấp phát cho mảng cơ sở (arr), size luôn luôn <= cap.
+ *
+ * @param v Con trỏ tới đối tượng vec-tơ (có kiểu gvec_t).
+ */
 #define gvec_size(v) (arr_size((v)->arr))
+
+/**
+ * Trả về dung lượng (capacity) của vec-tơ. Trong vec-tơ size là
+ * số lượng phần tử mảng đã sử dụng, còn capacity là số lượng phần tử
+ * đã cấp phát cho mảng cơ sở (arr), size luôn luôn <= cap.
+ *
+ * @param v Con trỏ tới đối tượng vec-tơ (có kiểu gvec_t).
+ */
 #define gvec_capacity(v) (arr_capacity((v)->arr))
 #define gvec_arr(v) (ARR((v)->arr))
 #define gvec_elem(v, i) ((gvec_arr(v))[(i)])
