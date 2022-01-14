c362b90998821d627273d9edcfb7dcf778846308
bangoc <bangoc@users.noreply.github.com>
2022-01-14 09:57:30 +0000

    deploy: e8c6df02a8ebf65ea7a6c87e070d759b27e36803

--- search/all_6.js
+++ search/all_6.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['rbm_2eh_0',['rbm.h',['../rbm_8h.html',1,'']]],
   ['rbm_5fcreate_1',['rbm_create',['../structrbm__s.html#a14819d682b69c1597dd2bbb3705f7c5a',1,'rbm_s']]],
-  ['rbm_5ffree_2',['rbm_free',['../rbm_8h.html#a06feb62cac7b623a27d034ffec7ee4e8',1,'rbm.h']]],
+  ['rbm_5ffree_2',['rbm_free',['../rbm_8h.html#a09233e201dbb5c07bb8dc0149b16dbde',1,'rbm.h']]],
   ['rbm_5finsert_3',['rbm_insert',['../structrbm__s.html#a9b5419361ac0c227a505d5a5868e6d2a',1,'rbm_s']]],
   ['rbm_5finsert_5fresult_4',['rbm_insert_result',['../structrbm__insert__result.html',1,'']]],
   ['rbm_5fires_5',['rbm_ires',['../rbm_8h.html#a8b6dcca52c60b3d20adf9d2ed22757ce',1,'rbm.h']]],
