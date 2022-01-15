f431cfb51f95025baa82ce14af093ddd264ed285
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-01-15 13:55:06 +0700

    Minh họa sử dụng gvec với số, chuỗi, và cấu trúc

---
+++ examples/gvec/gvec_i_demo.c
@@ -0,0 +1,30 @@
+/*
+  (C) Nguyen Ba Ngoc 2022
+  Minh họa sắp xếp mảng số nguyên với gvec
+*/
+
+#include "cgen.h"
+
+#include <stdio.h>
+
+int main() {
+  printf("Nhập các số không âm, hoặc nhập -1 để kết thúc: \n");
+  gvec_t vec = gvec_create(0, NULL);
+  int x;
+  for (;;) {
+    scanf("%d", &x);
+    if (x < 0) {
+      break;
+    }
+    gvec_append(vec, gtype_l(x));
+  }
+  printf("Bạn đã nhập %ld giá trị không âm\n", gvec_size(vec));
+  gvec_qsort(vec, gtype_qsort_l);
+  printf("Các giá trị theo thứ tự tăng dần: ");
+  gvec_traverse(value, vec) {
+    printf(" %ld", value->l);
+  }
+  printf("\n");
+  gvec_free(vec);
+  return 0;
+}
---
+++ examples/gvec/gvec_s_demo.c
@@ -0,0 +1,36 @@
+/*
+  (C) Nguyen Ba Ngoc 2022
+  Minh họa sắp xếp vec-tơ chuỗi ký tự với gvec
+*/
+
+#include "cgen.h"
+#include "ext/io.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  printf("Nhập vào các chuỗi ký tự hoặc STOP để kết thúc: \n");
+  gvec_t vec = gvec_create(0, gtype_free_s);
+  char *line = NULL;
+  for (;;) {
+    if (!cgetline(&line, NULL, stdin)) {
+      printf("Kết thúc tệp hoặc lỗi nhập.\n");
+      break;
+    }
+    if (strcmp(line, "STOP\n") == 0) {
+      break;
+    }
+    gvec_append(vec, gtype_s(strdup(line)));
+  }
+  printf("Bạn đã nhập: %ld chuỗi ký tự\n", gvec_size(vec));
+  gvec_qsort(vec, gtype_qsort_s);
+  printf("Các chuỗi đã nhập theo thứ tự tăng dần: ");
+  gvec_traverse(value, vec) {
+    printf("%s", value->s);
+  }
+  printf("\n");
+  gvec_free(vec);
+  free(line);
+  return 0;
+}
