5c10f537d82d977f9875a63e45fc81f1bacc9d8f
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2022-01-17 15:33:16 +0700

    Ví dụ minh họa sử dụng rbs

---
+++ examples/rbs/rbs_demo.c
@@ -0,0 +1,22 @@
+/*
+  (C) Nguyen Ba Ngoc 2022
+  Minh họa tạo tập hợp với rbs
+*/
+
+#include "cgen.h"
+
+int main() {
+  rbs_t s = rbs_create(gtype_cmp_s, NULL);
+  rbs_insert(s, gtype_s("aaaaa"));
+  rbs_insert(s, gtype_s("bbbbb"));
+  rbs_insert(s, gtype_s("ccccc"));
+  rbs_insert(s, gtype_s("aaaaa"));
+  rbs_insert(s, gtype_s("ccccc"));
+  rbs_insert(s, gtype_s("bbbbb"));
+  printf("Số lượng giá trị duy nhất = %ld\n", rbs_size(s));
+  rbs_traverse(key, s) {
+    printf("%s\n", key->s);
+  }
+  rbs_free(s);
+  return 0;
+}
