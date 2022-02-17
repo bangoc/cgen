63a5c927615a3c6f924e633f85f3c5ac5427231d
bangoc <bangoc@users.noreply.github.com>
2022-02-17 16:38:44 +0000

    deploy: aaebe5c08dfea9c169dcd11386af8248ac6791c8

--- search/all_3.js
+++ search/all_3.js
@@ -12,13 +12,14 @@ var searchData=
   ['gvec_5fappend_9',['gvec_append',['../gvec_8h.html#a471f5284a298bd1ede14e678d13e1ef8',1,'gvec.h']]],
   ['gvec_5farr_10',['gvec_arr',['../gvec_8h.html#acab1c1da0ec33cc19ead6f0e770742ab',1,'gvec.h']]],
   ['gvec_5fcapacity_11',['gvec_capacity',['../gvec_8h.html#ad212ac716ca79436ae5ba87cdf36fd5a',1,'gvec.h']]],
-  ['gvec_5felem_12',['gvec_elem',['../gvec_8h.html#ac4897cd3b7045cb4057ebbcac2a1581c',1,'gvec.h']]],
-  ['gvec_5ffree_13',['gvec_free',['../gvec_8h.html#a00376f59d46ad6c109d974a74894b8e1',1,'gvec.h']]],
-  ['gvec_5fidx_5fof_14',['gvec_idx_of',['../gvec_8h.html#a3fb42b68c0ceca57fafc13ded6140a06',1,'gvec.h']]],
-  ['gvec_5fqsort_15',['gvec_qsort',['../gvec_8h.html#a02cc2f9111065ed3c3a823ce47ee0fe3',1,'gvec.h']]],
-  ['gvec_5fremove_16',['gvec_remove',['../gvec_8h.html#a5f49bd4862c66d903edb32c776c4f6bb',1,'gvec.h']]],
-  ['gvec_5fs_17',['gvec_s',['../gvec_8h.html#a97885d6d1c5160da03ae767978381c5f',1,'gvec.h']]],
-  ['gvec_5fset_5fcapacity_18',['gvec_set_capacity',['../gvec_8h.html#a435902fa71c91fc2375d652e71a00166',1,'gvec.h']]],
-  ['gvec_5fsize_19',['gvec_size',['../gvec_8h.html#aa8ae641caf214f3f9e3da46f1b3e868e',1,'gvec.h']]],
-  ['gvec_5ftraverse_20',['gvec_traverse',['../gvec_8h.html#a3fb514600bc1e340c50412006acbd0c0',1,'gvec.h']]]
+  ['gvec_5fclear_12',['gvec_clear',['../gvec_8h.html#ae3b2488fc4b64421434658250bd4116f',1,'gvec.h']]],
+  ['gvec_5felem_13',['gvec_elem',['../gvec_8h.html#ac4897cd3b7045cb4057ebbcac2a1581c',1,'gvec.h']]],
+  ['gvec_5ffree_14',['gvec_free',['../gvec_8h.html#a00376f59d46ad6c109d974a74894b8e1',1,'gvec.h']]],
+  ['gvec_5fidx_5fof_15',['gvec_idx_of',['../gvec_8h.html#a3fb42b68c0ceca57fafc13ded6140a06',1,'gvec.h']]],
+  ['gvec_5fqsort_16',['gvec_qsort',['../gvec_8h.html#a02cc2f9111065ed3c3a823ce47ee0fe3',1,'gvec.h']]],
+  ['gvec_5fremove_17',['gvec_remove',['../gvec_8h.html#a5f49bd4862c66d903edb32c776c4f6bb',1,'gvec.h']]],
+  ['gvec_5fs_18',['gvec_s',['../gvec_8h.html#a97885d6d1c5160da03ae767978381c5f',1,'gvec.h']]],
+  ['gvec_5fset_5fcapacity_19',['gvec_set_capacity',['../gvec_8h.html#a435902fa71c91fc2375d652e71a00166',1,'gvec.h']]],
+  ['gvec_5fsize_20',['gvec_size',['../gvec_8h.html#aa8ae641caf214f3f9e3da46f1b3e868e',1,'gvec.h']]],
+  ['gvec_5ftraverse_21',['gvec_traverse',['../gvec_8h.html#a3fb514600bc1e340c50412006acbd0c0',1,'gvec.h']]]
 ];
