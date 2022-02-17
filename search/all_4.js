135ca2c44b971a2f008b39428bb4de6cd0795da2
bangoc <bangoc@users.noreply.github.com>
2022-02-17 18:46:19 +0000

    deploy: 75f04ffb1aa1fae58f128db8d6a346d8279ca16b

--- search/all_4.js
+++ search/all_4.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['hash_5fmap_0',['hash_map',['../structhash__map.html',1,'']]],
   ['hmap_2eh_1',['hmap.h',['../hmap_8h.html',1,'']]],
-  ['hmap_5ffree_2',['hmap_free',['../hmap_8h.html#aafdcb9f6bab54257cb101246b730bf49',1,'hmap.h']]],
-  ['hmap_5fgtype_5ffree_3',['hmap_gtype_free',['../hmap_8h.html#aa8b020bf60912fe2595bb23ad5e71e22',1,'hmap.h']]],
+  ['hmap_5fclear_2',['hmap_clear',['../hmap_8h.html#a6ad0aeae30cec73fdbf3262ed0061183',1,'hmap.h']]],
+  ['hmap_5ffree_3',['hmap_free',['../hmap_8h.html#aafdcb9f6bab54257cb101246b730bf49',1,'hmap.h']]],
   ['hmap_5finsert_5fresult_4',['hmap_insert_result',['../structhmap__insert__result.html',1,'']]],
   ['hmap_5fires_5',['hmap_ires',['../hmap_8h.html#ad6ee48932dfc13546a3f665f60b5fa08',1,'hmap.h']]],
   ['hmap_5fs_6',['hmap_s',['../hmap_8h.html#a60bbc23f7cdd178d7b190a7a71744fae',1,'hmap.h']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hmap_5fgtype_5ffree_0',['hmap_gtype_free',['../hmap_8h.html#aa8b020bf60912fe2595bb23ad5e71e22',1,'hmap.h']]]
+  ['hmap_5fclear_0',['hmap_clear',['../hmap_8h.html#a6ad0aeae30cec73fdbf3262ed0061183',1,'hmap.h']]]
 ];
