6663fc301d2453386f7e8f986908b13468bb19c9
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2023-10-15 06:05:46 +0700

    Triển khai danh sách liên kết đôi

--- v2/all.c
+++ v2/all.c
@@ -1,4 +1,5 @@
 #include "cont/queue.c"
+#include "cont/dlist.c"
 #include "cont/slist.c"
 #include "cont/vector.c"
 #include "utils/str.c"
--- v2/all.h
+++ v2/all.h
@@ -4,6 +4,7 @@
 #include "base/core.h"
 #include "base/gtype.h"
 #include "cont/queue.h"
+#include "cont/dlist.h"
 #include "cont/slist.h"
 #include "cont/vector.h"
 #include "utils/io.h"
