2bcffefe3457719c15d29391e13543761e890687
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2023-12-21 16:45:10 +0700

    Demo ngăn xếp và hàng đợi dựa trên danh sách móc nối đơn

---
+++ meta/tests/cgen/list_stack_queue_demo.c
@@ -0,0 +1,30 @@
+#include "cgen.h"
+SDECL_IMPL(list, char *, s);
+void demo_stack() {
+  printf("Demo stack: ==== \n");
+  struct list *stk = list();
+  spush(stk, "aaaaa");
+  spush(stk, "bbbbbbbb");
+  spush(stk, "ccccccccccc");
+  while (!sempty(stk)) {
+    printf("%s\n", *stop(stk));
+    spop(stk);
+  }
+  sfree(stk);
+}
+void demo_queue() {
+  printf("Demo queue: =======\n");
+  struct list *q = list();
+  senque(q, "aaaaaaaaaaa");
+  senque(q, "bbbbbbb");
+  senque(q, "ccccccccccccccccc");
+  while (!sempty(q)) {
+    printf("%ld: %s\n", ssize(q), *speek(q));
+    sdeque(q);
+  }
+  sfree(q);
+}
+int main() {
+  demo_stack();
+  demo_queue();
+}
