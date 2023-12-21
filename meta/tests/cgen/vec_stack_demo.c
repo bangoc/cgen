e80e000fa88becc6124fd534b76e33698ce08337
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2023-12-21 16:34:04 +0700

    demo stack dựa trên vec-tơ

---
+++ meta/tests/cgen/vec_stack_demo.c
@@ -0,0 +1,27 @@
+#include "cgen.h"
+
+VDECL_IMPL(ivector, int, vi);
+VDECL_IMPL(svector, char *, vs);
+void demo_vector_str() {
+  struct svector *v = svector(0);
+  vspush(v, "aaaaaa");
+  vspush(v, "bbbbbbbbbb");
+  vspush(v, "cccccccc");
+  while (!vsempty(v)) {
+    printf("%s\n", *vstop(v));
+    vspop(v);
+  }
+  vsfree(v);
+}
+int main() {
+  struct ivector *stk = ivector(0);
+  vipush(stk, 3);
+  vipush(stk, 5);
+  vipush(stk, 7);
+  while (!viempty(stk)) {
+    printf("%d\n", *vitop(stk));
+    vipop(stk);
+  }
+  vifree(stk);
+  demo_vector_str();
+}
