dbc809d6cba00bcc6748d76b758b8d0c5335f657
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2023-12-21 17:22:09 +0700

    Minh họa ánh xạ si

---
+++ meta/tests/cgen/si_demo.c
@@ -0,0 +1,25 @@
+/* Đếm tần suất từ */
+
+#include "cgen.h"
+
+TDECL_IMPL(simap, char *, int, si);
+
+int main() {
+  char key[100];
+  struct simap *si = simap(cmps);
+  si->fk = frees;
+  while (scanf("%s", key) == 1) {
+    int *value = siget(si, key);
+    if (value) {
+      ++(*value);
+    } else {
+      siput(si, strdup(key), 1);
+    }
+  }
+  struct TNN(simap) *nd = sifirst_lnr(si);
+  while (nd) {
+    printf("%s: %d\n", nd->key, nd->value);
+    nd = sinext_lnr(nd);
+  }
+  sifree(si);
+}
