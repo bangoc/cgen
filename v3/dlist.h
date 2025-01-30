3e8b9fc898f92c9c8e2d64bf14af55eeedc34ed3
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2025-01-30 19:13:25 +0700

    Danh sach moc noi kep - Macro

---
+++ v3/dlist.h
@@ -0,0 +1,220 @@
+#ifndef DLIST_H_
+#define DLIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DLIST_DECL(sname, elem_t) \
+struct sname##_node; \
+struct sname; \
+struct sname##_node *sname##_node(elem_t value); \
+struct sname *sname(); \
+struct sname *sname##_append(struct sname *list, elem_t value); \
+struct sname *sname##_prepend(struct sname *list, elem_t value); \
+int sname##_ins(struct sname *list, elem_t value, int idx); \
+int sname##_insa(struct sname *list, elem_t value, struct sname##_node *node); \
+int sname##_insb(struct sname *list, elem_t value, struct sname##_node *node); \
+struct sname##_node *sname##_at(struct sname *list, int idx); \
+struct sname *sname##_dfirst(struct sname *list); \
+struct sname *sname##_dlast(struct sname *list); \
+struct sname *sname##_dnode(struct sname *list, struct sname##_node *node); \
+int sname##_del(struct sname *list, int idx); \
+int sname##_empty(struct sname *list); \
+void sname##_free(struct sname *list);
+
+#define DLIST_IMPL(sname, elem_t) \
+struct sname##_node { \
+  elem_t value; \
+  struct sname##_node *next; \
+  struct sname##_node *prev; \
+}; \
+struct sname { \
+  struct sname##_node *first; \
+  struct sname##_node *last; \
+  int size; \
+}; \
+\
+struct sname##_node *sname##_node(elem_t value) { \
+  struct sname##_node *nn = malloc(sizeof(struct sname##_node)); \
+  nn->value = value; \
+  nn->next = NULL; \
+  nn->prev = NULL; \
+  return nn; \
+} \
+struct sname *sname() { \
+  struct sname *dll = malloc(sizeof(struct sname)); \
+  dll->first = dll->last = NULL; \
+  dll->size = 0; \
+  return dll; \
+} \
+void sname##_free(struct sname *list) { \
+  struct sname##_node *p = list->first, *n; \
+  while (p) { \
+    n = p->next; \
+    free(p); \
+    p = n; \
+  } \
+  free(list); \
+} \
+struct sname *sname##_append(struct sname *list, elem_t value) {\
+  struct sname##_node *nn = sname##_node(value); \
+  if (!nn) { \
+    return NULL; \
+  } \
+  if (list->first == NULL) { \
+    list->first = list->last = nn; \
+  } else { \
+    list->last->next = nn; \
+    nn->prev = list->last; \
+    list->last = nn; \
+  } \
+  ++list->size; \
+  return list; \
+} \
+struct sname *sname##_prepend(struct sname *list, elem_t value) {\
+  struct sname##_node *node = sname##_node(value); \
+  if (!node) { \
+    return NULL; \
+  } \
+  if (list->first == NULL) { \
+    list->first = list->last = node; \
+  } else { \
+    node->next = list->first; \
+    list->first->prev = node; \
+    list->first = node; \
+  } \
+  ++list->size; \
+  return list; \
+} \
+int sname##_ins(struct sname *list, elem_t value, int idx) { \
+  if (!list || idx < 0 || idx > list->size) { \
+    return 1; \
+  } \
+  if (idx == 0) { \
+    sname##_prepend(list, value); \
+    return 0; \
+  } \
+  if (idx == list->size) { \
+    sname##_append(list, value); \
+    return 0; \
+  } \
+  struct sname##_node *p = sname##_at(list, idx - 1), *nn = sname##_node(value); \
+  nn->next = p->next; \
+  p->next->prev = nn; \
+  p->next = nn; \
+  nn->prev = p; \
+  ++list->size; \
+  return 0; \
+} \
+int sname##_insa(struct sname *list, elem_t value, struct sname##_node *pos) { \
+  if (!list) { \
+    return 1; \
+  } \
+  if (pos == list->last) { \
+    sname##_append(list, value); \
+    return 0; \
+  } \
+  if (!pos) { \
+    return 1; \
+  } \
+  struct sname##_node *nn = sname##_node(value); \
+  nn->next = pos->next; \
+  pos->next->prev = nn; \
+  pos->next = nn; \
+  nn->prev = pos; \
+  ++list->size; \
+  return 0; \
+} \
+int sname##_insb(struct sname *list, elem_t value, struct sname##_node *pos) { \
+  if (!list) { \
+    return 1; \
+  } \
+  if (pos == list->first) { \
+    sname##_prepend(list, value); \
+    return 0; \
+  } \
+  if (!pos) { \
+    return 1; \
+  } \
+  struct sname##_node *nn = sname##_node(value); \
+  nn->prev = pos->prev; \
+  pos->prev->next = nn; \
+  nn->next = pos; \
+  pos->prev = nn; \
+  ++list->size; \
+  return 0; \
+} \
+struct sname##_node *sname##_at(struct sname *list, int idx) { \
+  if (!list || idx < 0 || idx >= list->size) { \
+    return NULL; \
+  } \
+  struct sname##_node *n = list->first; \
+  for (int i = 1; i <= idx; ++i) { \
+    n = n->next; \
+  } \
+  return n; \
+} \
+struct sname *sname##_dfirst(struct sname *list) { \
+  if (!list || sname##_empty(list)) { \
+    return NULL; \
+  } \
+  if (list->first == list->last) { \
+    list->last = NULL; \
+  } \
+  struct sname##_node *tmp = list->first; \
+  list->first = tmp->next; \
+  free(tmp); \
+  --list->size; \
+  return list; \
+} \
+struct sname *sname##_dlast(struct sname *list) { \
+  if (!list || list->last == NULL) { \
+    return NULL; \
+  } \
+  if (list->first == list->last) { \
+    list->first = NULL; \
+  } \
+  struct sname##_node *tmp = list->last; \
+  list->last = tmp->prev; \
+  free(tmp); \
+  --list->size; \
+  return list; \
+} \
+struct sname *sname##_dnode(struct sname *list, struct sname##_node *node) { \
+  if (!node) { \
+    return NULL; \
+  } \
+  if (node == list->first) {  \
+    return sname##_dfirst(list); \
+  } \
+  if (node == list->last) { \
+    return sname##_dlast(list); \
+  } \
+  struct sname##_node *prev = node->prev, *next = node->next; \
+  prev->next = next; \
+  next->prev = prev; \
+  free(node); \
+  --list->size; \
+  return list; \
+} \
+int sname##_del(struct sname *list, int idx) { \
+  if (!list || idx < 0 || idx >= list->size) { \
+    return 1; \
+  } \
+  if (idx == 0) { \
+    sname##_dfirst(list); \
+    return 0; \
+  } \
+  struct sname##_node *n = sname##_at(list, idx); \
+  sname##_dnode(list, n); \
+  return 0; \
+} \
+int sname##_empty(struct sname *list) { \
+  return list->first == NULL || list->last == NULL; \
+}
+
+#define DLIST_DECL_IMPL(sname, elem_t) \
+DLIST_DECL(sname, elem_t) \
+DLIST_IMPL(sname, elem_t)
+
+#endif  // DLIST_H_
---
+++ v3/tests/dlist_demo_ut.c
@@ -0,0 +1,85 @@
+#include "dlist.h"
+
+#include <stdio.h>
+
+DLIST_DECL_IMPL(dll, int)
+
+int main() {
+  struct dll *list = dll();
+  dll_append(list, 3);
+  dll_append(list, 5);
+  dll_prepend(list, 1);
+  for (struct dll_node *n = list->first; n; n = n->next) {
+    printf(" %d", n->value);
+  }
+  printf("\n");
+  for (struct dll_node *n = list->last; n; n = n->prev) {
+    printf(" %d", n->value);
+  }
+  printf("\n");
+  if (list->size != 3) {
+    printf("size = %d - error\n", list->size);
+    return 1;
+  }
+  dll_ins(list, 7, 3);
+  dll_ins(list, 0, 0);
+  int a[] = {0, 1, 3, 5, 7};
+  if (list->size != 5) {
+    printf("size = %d - error\n", list->size);
+    return 1;
+  }
+  int i = 0;
+  for (struct dll_node *n = list->first; n; n = n->next) {
+    if (n->value != a[i++]) {
+      printf("Value error at %d = %d\n", i, n->value);
+      return 1;
+    }
+  }
+  i = 4;
+  for (struct dll_node *n = list->last; n; n = n->prev) {
+    if (n->value != a[i--]) {
+      printf("Value error at %d = %d\n", i, n->value);
+      return 1;
+    }
+  }
+  dll_insa(list, 7, list->first);
+  dll_insb(list, 9, list->last);
+  dll_insb(list, 11, list->last);
+
+  for (struct dll_node *n = list->first; n; n = n->next) {
+    printf(" %d", n->value);
+  }
+  printf("\n");
+
+  // 0 7 1 3 5 9 11 7
+  dll_dfirst(list);
+  dll_dlast(list);
+  dll_dnode(list, list->last);
+
+  // 7 1 3 5 9
+  dll_del(list, 1);
+  dll_insb(list, 5, list->first);
+  dll_insa(list, 15, list->last);
+
+  int b[] = {5, 7, 3, 5, 9, 15};
+  if (list->size != 6) {
+    printf("Size = %d -- error\n", list->size);
+    return 1;
+  }
+  i = 0;
+  for (struct dll_node *n = list->first; n; n = n->next) {
+    if (n->value != b[i++]) {
+      printf("Value error at %d = %d\n", i, n->value);
+      return 1;
+    }
+  }
+  i = 5;
+  for (struct dll_node *n = list->last; n; n = n->prev) {
+    if (n->value != b[i--]) {
+      printf("Value error at %d = %d\n", i, n->value);
+      return 1;
+    }
+  }
+  printf("Test Ok\n");
+  dll_free(list);
+}
